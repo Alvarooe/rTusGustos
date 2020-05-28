87acaf6025a79f41307154ec853a681292254dbb
Alvarooe <aesalasg@yahoo.com>
2020-05-28 09:41:30 -0500

    actualiza AppRest

---
+++ AppRest/app/src/main/java/com/example/apprest/A06xMenuFragment.java
@@ -0,0 +1,31 @@
+package com.example.apprest;
+
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class A06xMenuFragment extends Fragment {
+
+
+    public A06xMenuFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_a06x_menu, container, false);
+    }
+
+}
